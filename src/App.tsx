7dfeb81d64c0809e8fbccb6307bb13bfb15ce426
berryboylb <phemmynesce4life@gmail.com>
2023-04-26 02:13:36 +0100

    @chore :l

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { Home, NotFound, initFontAwesome } from "./pages";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Spinner from "./components/Spinner/Spinner";
+// import Spinner from "./components/Spinner/Spinner";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient({});
+const Spinner = lazy(() => import("./components/Spinner/Spinner"));
 initFontAwesome();
 function App() {
   const router = createBrowserRouter([
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { BounceLoader, PacmanLoader } from "react-spinners";
 import { useMediaQuery } from "react-responsive";
+import { BounceLoader, PacmanLoader } from "react-spinners";
 type Props = {
   toggle?: boolean;
 };
